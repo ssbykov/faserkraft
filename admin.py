3413654caa003dbe36cdaabd8ce420132bf86dc1
Sergei Bykov <ss.bykoff@gmail.com>
2023-04-28 15:27:04 +0300

    Вариант моделей со связью м2м чрезе объект Table

--- admin.py
+++ admin.py
@@ -3,7 +3,7 @@ from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 
-from models import User, db
+from models import User, db, Stage, Product#, Execution, Operation, Unit
 
 
 def user_check(current_user):
@@ -25,7 +25,34 @@ class MyModelView(ModelView):
     def inaccessible_callback(self, name, **kwargs):
          return redirect(url_for('profile', next=request.url))
 
+# class ChildView(ModelView):
+    # form_columns = ('id',)
+    # column_display_all_relations = True
+    # inline_models = (
+    #     Product,)
+    # form_columns = ['product_id']
+    # column_list = [c_attr.key for c_attr in db.inspect(Technology).mapper.column_attrs]
+    # column_list = ('id', 'product_id', 'stage_id', 'execution_id')
+
+class ProductAdmin(ModelView):
+    column_labels = dict(name='Название', stages='Этап')
+    # column_display_all_relations = True
+    # column_list = [c_attr.key for c_attr in db.inspect(Technology).mapper.column_attrs]
+    column_list = ['id', 'name']
+    form_columns = ['name', 'stages']
+
+# class TechnologyAdmin(ModelView):
+#     column_display_all_relations = True
+#     # column_list = [c_attr.key for c_attr in db.inspect(Technology).mapper.column_attrs]
+#     column_list = ['id', 'product_id']
+#     form_columns = ['id', 'product', ]
 
 admin = Admin(index_view=MyAdminIndexView(), name='Фазеркрафт', endpoint='admin')
 admin.add_view(MyModelView(User, db.session, name='Пользователь'))
+admin.add_view(ProductAdmin(Product, db.session, name='Продукт'))
+# admin.add_view(TechnologyAdmin(Technology, db.session, name='Технология'))
+# admin.add_view(OperationAdmin(Operation, db.session, name='Операция'))
+admin.add_view(MyModelView(Stage, db.session, name='Технологический этап'))
+# admin.add_view(MyModelView(Execution, db.session, name='Операция'))
+# admin.add_view(MyModelView(Unit, db.session, name='Изделие'))
 
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
+from flask_jwt_extended import create_access_token
 from flask_sqlalchemy import SQLAlchemy
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -38,11 +39,87 @@ class User(db.Model):
     def __repr__(self):
         return f"<users {self.id}>"
 
+    def get_token(self, expire_time=12):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(
+            identity=self.id, expires_delta=expire_delta)
+        return token
+
+
+product_stage = db.Table('product_stage',
+                         db.Column('order_num', db.Integer),
+                         db.Column('product_id', db.Integer, db.ForeignKey('product.id')),
+                         db.Column('stage_id', db.Integer, db.ForeignKey('stage.id'))
+                         )
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(255), nullable=True)
+    stages = db.relationship("Stage", secondary=product_stage, back_populates="products")
+
+    def __unicode__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class Stage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    products = db.relationship("Product", secondary=product_stage, back_populates="stages")
+
+    def __repr__(self):
+        return self.name
+
+# class Operation(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     num = db.Column(db.Integer, unique=True)
+#     name = db.Column(db.String(100), nullable=False, unique=True)
+#     operation = db.relationship('Operation', backref=db.backref('execution', lazy='dynamic'))
+
+
+# class Operation(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     stage_id = db.Column(db.Integer, db.ForeignKey(Stage.id), nullable=False)
+#     execution_id = db.Column(db.Integer, db.ForeignKey(Execution.id), nullable=False)
+#     db.UniqueConstraint(stage_id, execution_id)
+# product = db.relationship(Product, backref='operations')
+# execution = db.relationship(Execution, back_populates='technologes')
+
+
+# class Technology(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     product_id = db.Column(db.Integer, db.ForeignKey(Product.id), nullable=False)
+#     operation_id = db.Column(db.Integer, db.ForeignKey(Operation.id), nullable=False)
+# product = db.relationship(Product, backref='operations')
+# execution = db.relationship(Execution, back_populates='technologes')
+
+
+# class Unit(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     product_id = db.Column(db.Integer, db.ForeignKey(Product.id), nullable=False)
+# product = db.relationship("Product", backref='unit')
+
+
+# class Event(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     unit_id = db.Column(db.Integer, db.ForeignKey(Unit.id), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+# technology_id = db.Column(db.Integer, db.ForeignKey(Technology.id), nullable=False)
+# @db.validates('stage_execution_id')
+# def validate_name(self, key, value):
+#     stage_execution = db.session.query()
+#     assert value != ''
+#     return value
+
 
 class DB:
 
-    def get_user(self, session, user_id):
-        user = session.query(User).get(user_id)
+    def get_user(self, user_id):
+        user = db.session.query(User).get(user_id)
         if user is None:
             raise HttpError(404, 'Пользователя не существуйет!')
         return user
